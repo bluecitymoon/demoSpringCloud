82f7d44a1453846b834590293976ccd8336c276c
Jerry <leeming-jiang@163.com>
2017-06-14 14:21:10 +0800

    use rabbitmq as config messge bus

--- config/src/main/java/com/jerry/config/ConfigApplication.java
+++ config/src/main/java/com/jerry/config/ConfigApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * http://tech.asimio.net/2017/02/02/Refreshable-Configuration-using-Spring-Cloud-Config-Server-Spring-Cloud-Bus-RabbitMQ-and-Git.html
+ */
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
